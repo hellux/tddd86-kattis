316c0cb2ab4a36c69d60f4d6ae75528a5812f6f6
Hellux <hellux07@gmail.com>
2017-09-09 14:56:21 +0200

    added solution in c

--- ferryloading4/fl.c
+++ ferryloading4/fl.c
@@ -1,13 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_CARS 10000
 
-void sail(int* trips, char* bank, int* space, int length) {
-    *(trips) += 1;
-    *bank = *bank == 'l' ? 'r' : 'l';
-    *space = length;
-}
+struct queue left;
+struct queue right;
+
+/*
+ * Queue with consant memory block
+ * push and pop by modifying start and end indicators
+ * - end points to address after first element
+ * - start points to first element (end if empty)
+ */
+struct queue {
+    int cars[MAX_CARS];
+    int start;
+    int end;
+};
+struct queue* queues(char bank) { return bank == 'l' ? &left : &right; }
+void reset(struct queue* q) { q->start = 0; q->end = 0; }
+bool nonempty(struct queue* q) { return (q->end - q->start) != 0; }
+void push(struct queue* q, int car) { q->cars[q->end++] = car; }
+void pop(struct queue* q) { q->start++; }
+int front(struct queue* q) { return q->cars[q->start]; }
 
 int main(int argc, char* args[]) {
     int tests;
@@ -19,29 +34,31 @@ int main(int argc, char* args[]) {
         scanf("%d%d", &length, &cars);
         length *= 100;
 
-        char bank = 'l';
-        int space = length;
-        int trips = 0;
-        //int buffer_l[MAX_CARS];
-        //int buffer_r[MAX_CARS];
+        reset(queues('l'));
+        reset(queues('r'));
         for (int c = 0; c < cars; c++) {
             int car_length;
             char car_bank[6];
             scanf("%d%s", &car_length, car_bank);
-
-            if (car_bank[0] == bank) {
-                if (car_length <= space) {
-                    space -= car_length;
-                } else {
-                    sail(&trips, &bank, &space, length);
-                }
+            push(queues(car_bank[0]), car_length);
+        }
+        
+        char bank = 'l';
+        int space = length;
+        int trips = 1;
+        while (nonempty(queues('l')) || nonempty(queues('r'))) {
+            if (nonempty(queues(bank)) && front(queues(bank)) <= space) {
+                space -= front(queues(bank));
+                pop(queues(bank));
             } else {
-                sail(&trips, &bank, &space, length);
+                bank = (bank == 'l' ? 'r' : 'l');
+                trips++;
+                space = length;
             }
         }
 
         printf("%d\n", trips);
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
