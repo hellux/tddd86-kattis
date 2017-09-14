b9336005894b3a59f044d0ae925b32e29d11c046
Hellux <hellux07@gmail.com>
2017-09-14 19:37:17 +0200

    tried optimizing

--- cd/cd.c
+++ cd/cd.c
@@ -1,53 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define MAX_CDS 1000000
 
 /*
  * Return pos of value in sorted array (if in array, otherwise where it would be)
  */
-unsigned long pos(unsigned long array[],
-                  unsigned long start,
-                  unsigned long end,
-                  unsigned long value) {
+unsigned pos(unsigned array[], unsigned start, unsigned end, unsigned value, bool* found) {
     while (start < end) {
-        unsigned long middle = (start + end) / 2;
-        unsigned long current = array[middle];
+        unsigned middle = (start + end) >> 1;
+        unsigned current = array[middle];
 
         if (current < value) {
             start = middle + 1;
         } else if (value < current) {
             end = middle;
         } else {
+            *found = true;
             return middle;
         }
     }
+
     return start;
 }
 
 int main(int argc, char* args[]) {
-    unsigned long jack_catalog[MAX_CDS];
-    unsigned long jack, jill;
-    while(scanf("%lu%lu", &jack, &jill) == 2) {
+    unsigned jack_catalog[MAX_CDS];
+    unsigned jack, jill;
+    char buffer[10000];
+    unsigned buffer_size = 0;
+
+    while(scanf("%u%u", &jack, &jill) == 2) {
         if (jack == 0 && jill == 0) break;
-        unsigned long start = 0;
-        unsigned long size;
+
+        unsigned size;
         for (size = 0; size < jack; size++) {
-            scanf("%lu", jack_catalog+size);
+            scanf("%u", jack_catalog+size);
         }
 
-        unsigned long sell = 0;
-        for (unsigned long i = 0; i < jill; i++) {
-            unsigned long cat_num;
-            scanf("%lu", &cat_num);
-            start = pos(jack_catalog, start, size, cat_num);
-            if (jack_catalog[start] == cat_num) {
+        unsigned start = 0;
+        unsigned sell = 0;
+        for (unsigned i = 0; i < jill; i++) {
+            unsigned cat_num;
+            scanf("%u", &cat_num);
+
+            bool found = false;
+            start = pos(jack_catalog, start, size, cat_num, &found);
+            if (found) {
                 sell++;
             }
         }
-
-        printf("%lu\n", sell);
+        
+        sprintf(buffer+buffer_size, "%u", sell);
+        buffer_size += strlen(buffer+buffer_size)+1;
+        buffer[buffer_size-1] = '\n';
     }
+    
+    buffer[buffer_size] = '\0';
+    printf(buffer);
 
     return EXIT_SUCCESS;
 }
--- cd/cd.cpp
+++ cd/cd.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 int main() {
-    unsigned long jack, jill;
+    unsigned jack, jill;
 
     while (cin >> jack >> jill) {
         if (jack == 0 && jill == 0) break;
 
-        set<unsigned long> jack_catalog;
+        set<unsigned> jack_catalog;
 
-        for (unsigned long i = 0; i < jack; i++) {
-            unsigned long cat_num;
+        for (unsigned i = 0; i < jack; i++) {
+            unsigned cat_num;
             cin >> cat_num;
             jack_catalog.insert(cat_num);
         }
 
-        unsigned long sell = 0;
-        for (unsigned long i = 0; i < jill; i++) {
-            unsigned long cat_num;
+        unsigned sell = 0;
+        for (unsigned i = 0; i < jill; i++) {
+            unsigned cat_num;
             cin >> cat_num;
             if (jack_catalog.find(cat_num) != jack_catalog.end()) {
                 sell++;
             }
         }
 
-        cout << sell << endl;
+        cout << sell << '\n';
     }
 }
